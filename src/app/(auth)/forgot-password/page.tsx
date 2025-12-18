fbb207f955af267d41a6a9a9dedd57f9f5ff640d
Cole <aderibigbeadedamolajohn@gmail.com>
2025-12-18 23:52:41 +0100

    refactor: Rename and enhance the forgot password page component for improved clarity and user experience.

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
-};
-
-export default page;
+export default function ForgotPasswordPage() {
+  return <div>ForgotPasswordPage</div>;
+}
