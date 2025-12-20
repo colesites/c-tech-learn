e0df3533055e5340e0d32779c15b48c720421a0a
Cole <aderibigbeadedamolajohn@gmail.com>
2025-12-20 11:03:49 +0100

    refactor: Adjust loading components in sign-in and sign-up pages to utilize dynamic viewport height for enhanced layout responsiveness.

--- src/app/(auth)/sign-in/loading.tsx
+++ src/app/(auth)/sign-in/loading.tsx
@@ -2,7 +2,7 @@ import { Spinner } from "@/components/ui/spinner";
 
 const loading = () => {
   return (
-    <div className="flex h-full w-full items-center justify-center bg-background">
+    <div className="flex h-dvh lg:h-full w-full items-center justify-center bg-background">
       <Spinner className="size-10 text-primary" />
     </div>
   );
